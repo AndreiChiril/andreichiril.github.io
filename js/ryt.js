8a44781d8c052447cdbd7b1ffc4772a527433118
AndreiChiril <72802080+AndreiChiril@users.noreply.github.com>
2021-06-26 16:25:42 +0300

    Update ryt.js

--- js/ryt.js
+++ js/ryt.js
@@ -11,17 +11,17 @@ var a10 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D",
 var a11 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", 'H', "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", 'h', "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", "_"];
 
 function ryt() {
-	var rand1 = Math.floor(Math.random() * 64);
-	var rand2 = Math.floor(Math.random() * 64);
-	var rand3 = Math.floor(Math.random() * 64);
-	var rand4 = Math.floor(Math.random() * 64);
-	var rand5 = Math.floor(Math.random() * 64);
-	var rand6 = Math.floor(Math.random() * 64);
-	var rand7 = Math.floor(Math.random() * 64);
-	var rand8 = Math.floor(Math.random() * 64);
-	var rand9 = Math.floor(Math.random() * 64);
-	var rand10 = Math.floor(Math.random() * 64);
-	var rand11 = Math.floor(Math.random() * 64);
+	var rand1 = Math.floor(Math.random() * 65);
+	var rand2 = Math.floor(Math.random() * 65);
+	var rand3 = Math.floor(Math.random() * 65);
+	var rand4 = Math.floor(Math.random() * 65);
+	var rand5 = Math.floor(Math.random() * 65);
+	var rand6 = Math.floor(Math.random() * 65);
+	var rand7 = Math.floor(Math.random() * 65);
+	var rand8 = Math.floor(Math.random() * 65);
+	var rand9 = Math.floor(Math.random() * 65);
+	var rand10 = Math.floor(Math.random() * 65);
+	var rand11 = Math.floor(Math.random() * 65);
 	var ied = a1[rand1] + a2[rand2] + a3[rand3] + a4[rand4] + a5[rand5] + a6[rand6] + a7[rand7] + a8[rand8] + a9[rand9] + a10[rand10] + a11[rand11];
 	document.getElementById("demo").innerHTML = 'Video ID: ' + ied;
 	document.getElementById("demo1").innerHTML = '<div class="container"><iframe class="responsive-iframe" src="https://www.youtube-nocookie.com/embed/' + ied + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
