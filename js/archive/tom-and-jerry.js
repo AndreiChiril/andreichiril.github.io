d11ba0f98a87a665a6366875845aec4c0e21e04d
AndreiChiril <72802080+AndreiChiril@users.noreply.github.com>
2021-07-06 16:35:55 +0300

    Update and rename js/tom-and-jerry.js to js/archive/tom-and-jerry.js

---
+++ js/archive/tom-and-jerry.js
@@ -0,0 +1,3 @@
+function taj1() {
+	document.getElementById("taj1").innerHTML = '<div class="container"><iframe class="responsive-iframe" src="https://drive.google.com/file/d/1pxdlp3KWVPix1qwXvcRpmBhdngg8FYYj/preview"></iframe></div>';
+}
