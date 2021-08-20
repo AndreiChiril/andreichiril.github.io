8029db543ba493a7a35e49c1e559ef92b0166f8a
AndreiChiril <72802080+AndreiChiril@users.noreply.github.com>
2021-08-20 16:38:57 +0300

    Delete js/archive directory

--- js/archive/tom-and-jerry.js
+++
@@ -1,3 +0,0 @@
-function taj1() {
-	document.getElementById("taj1").innerHTML = '<div class="container"><iframe class="responsive-iframe" src="https://drive.google.com/file/d/1pxdlp3KWVPix1qwXvcRpmBhdngg8FYYj/preview"></iframe></div>';
-}
